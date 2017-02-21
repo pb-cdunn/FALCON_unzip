8da3fe10440fb1de571d9dfded5e2a781d5964da
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-02-21 17:26:35 -0600

    Fix SAT-828: Unzip race-condition (#5)
    - Drop pypeflow for sub-job.
    - Default paths for argparse
    - Note to run in base-dir
    
    https://jira.pacificbiosciences.com/browse/SAT-827

--- falcon_unzip/get_read_hctg_map.py
+++ falcon_unzip/get_read_hctg_map.py
@@ -1,5 +1,3 @@
-from pypeflow.simple_pwatcher_bridge import (PypeProcWatcherWorkflow, MyFakePypeThreadTaskBase,
-        makePypeLocalFile, fn, PypeTask)
 import argparse
 import logging
 import os
@@ -9,27 +7,27 @@ def make_dirs(d):
     if not os.path.isdir(d):
         os.makedirs(d)
 
-def generate_read_to_hctg_map(self):
-    rawread_id_file = fn( self.rawread_id_file )
-    pread_id_file = fn( self.pread_id_file )
-    read_to_contig_map = fn( self.read_to_contig_map )
+def generate_read_to_hctg_map(
+        rawread_ids_fn, pread_ids_fn,
+        h_ctg_ids_fn,
+        h_ctg_edges_fn,
+        p_ctg_edges_fn,
+        read_to_contig_map_fn):
+    make_dirs(os.path.dirname(read_to_contig_map_fn)) # Workflow does this too.
 
-    pread_did_to_rid = open(pread_id_file).read().split('\n')
-    rid_to_oid = open(rawread_id_file).read().split('\n')
-
-    h_ctg_edges = fn( self.h_ctg_edges )
-    p_ctg_edges = fn( self.p_ctg_edges )
+    pread_did_to_rid = open(pread_ids_fn).read().split('\n')
+    rid_to_oid = open(rawread_ids_fn).read().split('\n')
 
     h_ctg_ids = set()
-    with open(fn(self.h_ctg_ids)) as f:
+    with open(h_ctg_ids_fn) as f:
         for row in f:
             row = row.strip()
             h_ctg_ids.add( row )
 
     pread_to_contigs = {}
 
-    for fnanme in ( p_ctg_edges, h_ctg_edges):
-        with open(fnanme) as f:
+    for fnname in (p_ctg_edges_fn, h_ctg_edges_fn):
+        with open(fnname) as f:
             for row in f:
                 row = row.strip().split()
                 ctg = row[0]
@@ -52,57 +50,45 @@ def generate_read_to_hctg_map(self):
                 pread_to_contigs.setdefault( k2, set() )
                 pread_to_contigs[ k2 ].add( ctg )
 
-    with open(read_to_contig_map, 'w') as f:
+    with open(read_to_contig_map_fn, 'w') as f:
         for k in pread_to_contigs:
             pid, rid, oid = k
             for ctg in list(pread_to_contigs[ k ]):
                 print >>f, '%09d %09d %s %s' % (pid, rid, oid, ctg)
 
-def get_read_hctg_map(asm_dir, hasm_dir, read_to_contig_map_fn):
-    wf = PypeProcWatcherWorkflow(
-            max_jobs=12, # TODO: Why was NumThreads ever set? There is only one task!
-    )
 
-    rawread_id_file = makePypeLocalFile(os.path.join(asm_dir, 'read_maps/dump_rawread_ids/rawread_ids'))
-    pread_id_file = makePypeLocalFile(os.path.join(asm_dir, 'read_maps/dump_pread_ids/pread_ids'))
-    h_ctg_edges = makePypeLocalFile(os.path.join(hasm_dir, 'all_h_ctg_edges'))
-    p_ctg_edges = makePypeLocalFile(os.path.join(hasm_dir, 'all_p_ctg_edges'))
-    h_ctg_ids = makePypeLocalFile( os.path.join(hasm_dir, "all_h_ctg_ids"))
-    #make_dirs(os.path.dirname(os.path.abspath(read_to_contig_map_fn)) # Workflow does this.
-
-    read_to_contig_map_plf = makePypeLocalFile(read_to_contig_map_fn)
-
-    inputs = { 'rawread_id_file': rawread_id_file,
-               'pread_id_file': pread_id_file,
-               'h_ctg_edges': h_ctg_edges,
-               'p_ctg_edges': p_ctg_edges,
-               'h_ctg_ids': h_ctg_ids}
-
-    make_task = PypeTask(
-               inputs = inputs,
-               outputs = {'read_to_contig_map': read_to_contig_map_plf},
-    )
-    wf.addTask(make_task(generate_read_to_hctg_map))
-    wf.refreshTargets() # block
-
-
+#RAWREAD_DIR = './0-rawreads'
+#PREAD_DIR = './1-preads_ovl'
+ASM_DIR = './2-asm-falcon'
+HASM_DIR = './3-unzip'
+QUIVER_DIR = './4-quiver'
 
 def parse_args(argv):
-    parser = argparse.ArgumentParser(description='generate `read_to_contig_map` that contains the \
-information from the chain of mapping: (contig id, last col) -> (internal p-read id) -> (internal raw-read id) -> (original read id)\n \
-it assumes the 2-asm-falcon/read_maps ... /dump_rawread_ids/rawread_ids and /dump_pread_ids/pread_ids are already generated',
+    description="""Generate `read_to_contig_map` that contains the
+information from the chain of mapping: (contig id, last col) -> (internal p-read id) -> (internal raw-read id) -> (original read id)
+It assumes the inputs are already generated.
+"""
+    epilog = """You should run this in the base run directory (where "./4-quiver/" lives) if you want to use default arguments."""
+    parser = argparse.ArgumentParser(description=description, epilog=epilog,
               formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--output', type=str, default="./4-quiver/read_maps/read_to_contig_map", help='output file')
+    parser.add_argument('--rawread-ids-fn', default=os.path.join(ASM_DIR, 'read_maps/dump_rawread_ids/rawread_ids'),
+            help='rawread_ids filename')
+    parser.add_argument('--pread-ids-fn', default=os.path.join(ASM_DIR, 'read_maps/dump_pread_ids/pread_ids'),
+            help='pread ids filename')
+    parser.add_argument('--p-ctg-edges-fn', default=os.path.join(HASM_DIR, 'all_p_ctg_edges'),
+            help='primary contig edges filename')
+    parser.add_argument('--h-ctg-edges-fn', default=os.path.join(HASM_DIR, 'all_h_ctg_edges'),
+            help='haplotype contig edges filename')
+    parser.add_argument('--h-ctg-ids-fn', default=os.path.join(HASM_DIR, 'all_h_ctg_ids'),
+            help='haplotype contig ids filename')
+    parser.add_argument('--read-to-contig-map-fn', default=os.path.join(QUIVER_DIR, 'read_maps/read_to_contig_map'),
+            help='OUTPUT: read_to_contig_map filename')
     args = parser.parse_args(argv[1:])
     return args
 
 def main(argv=sys.argv):
     logging.basicConfig()
-    args = parse_args(argv)
-    output = args.output
-    #rawread_dir = os.path.abspath('0-rawreads')
-    #pread_dir = os.path.abspath('1-preads_ovl')
-    asm_dir = os.path.abspath('2-asm-falcon')
-    hasm_dir = os.path.abspath('3-unzip')
 
-    get_read_hctg_map(asm_dir=asm_dir, hasm_dir=hasm_dir, read_to_contig_map_fn=output)
+    args = parse_args(argv)
+
+    generate_read_to_hctg_map(**vars(args))
