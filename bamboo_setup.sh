fd152b1c2461053930fa8c97e34f34483ce000ef
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-10-11 09:52:58 -0700

    pytest-cov; drop nose

--- bamboo_setup.sh
+++ bamboo_setup.sh
@@ -1,8 +1,10 @@
 type module >& /dev/null || . /mnt/software/Modules/current/init/bash
 
 module load gcc/6.4.0
-module load python/2.7.13-UCS2
-which pip
+module load ccache/3.2.3
+module load python/2.7.13-UCS4
+which python
+which pip # assumed
 
 export PYTHONUSERBASE=$(pwd)/LOCAL
 mkdir -p LOCAL/
