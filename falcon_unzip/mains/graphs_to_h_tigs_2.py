1796051c04d185d937b6fab7f7f99f07f326fd50
Christopher Dunn <cdunn@pacificbiosciences.com>
2018-03-15 07:28:38 -0700

    dashes instead of underscores in flags

--- falcon_unzip/mains/graphs_to_h_tigs_2.py
+++ falcon_unzip/mains/graphs_to_h_tigs_2.py
@@ -1074,18 +1074,18 @@ def parse_args(argv):
         description='layout haplotigs from primary assembly graph and phased aseembly graph')
 
     parser.add_argument(
-        '--fc_asm_path', type=str,
+        '--fc-asm-path', type=str,
         help='path to the primary Falcon assembly output directory', required=True)
     parser.add_argument(
-        '--fc_hasm_path', type=str,
+        '--fc-hasm-path', type=str,
         help='path to the phased Falcon assembly output directory', required=True)
     parser.add_argument(
-        '--ctg_id', type=str, help='contig identifier in the bam file', default="all", required=True)
+        '--ctg-id', type=str, help='contig identifier in the bam file', default="all", required=True)
     parser.add_argument(
-        '--base_dir', type=str, default="./",
+        '--base-dir', type=str, default="./",
         help='the output base_dir, default to current working directory')
     parser.add_argument(
-        '--rid_phase_map', type=str,
+        '--rid-phase-map', type=str,
         help="path to the file that encode the relationship of the read id to phase blocks", required=True)
     parser.add_argument(
         '--fasta', type=str, help="sequence file of the p-reads", required=True)
