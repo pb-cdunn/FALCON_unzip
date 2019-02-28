77a0ad3a211806c3c94bf0c103d55ece1e3700b5
Christopher Dunn <cdunn@pacificbiosciences.com>
2019-02-28 10:58:34 -0800

    micro bump to 1.1.7

--- falcon_unzip/__init__.py
+++ falcon_unzip/__init__.py
@@ -1,7 +1,7 @@
-__version__ = '1.1.6'
+__version__ = '1.1.7'
 
 try:
     import sys, pkg_resources
-    sys.stderr.write('{}\n'.format(pkg_resources.get_distribution('falcon-unzip')))
+    sys.stderr.write('falcon-unzip {} (pip thinks "{}")\n'.format(__version__, pkg_resources.get_distribution('falcon-unzip')))
 except Exception:
     pass
