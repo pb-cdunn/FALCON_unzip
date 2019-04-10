fe860881a988f1318e4936d972df350d31bd81ef
Christopher Dunn <cdunn@pacificbiosciences.com>
2019-04-10 08:16:37 -0700

    Add --target argument, defaulting to clr

--- falcon_unzip/mains/start_unzip.py
+++ falcon_unzip/mains/start_unzip.py
@@ -9,8 +9,12 @@ class HelpF(argparse.RawTextHelpFormatter, argparse.ArgumentDefaultsHelpFormatte
 
 def parse_args(argv):
     parser = argparse.ArgumentParser(
-        description='Run stage 3-unzip and stage 4-polish, given the results of stage 2-asm-falcon.',
+        description='Run stage 3-unzip and stage 4-polish, given the results of stage 2-asm-falcon of Falcon.',
         formatter_class=HelpF,
+    )
+    parser.add_argument(
+        '--target', default='clr', choices=['clr', 'ccs'],
+        help='target pipeline: clr=regular; ccs=only ccs data',
     )
     parser.add_argument(
         '--logging-config-fn',
@@ -18,7 +22,7 @@ def parse_args(argv):
     )
     parser.add_argument(
         'config_fn', type=str,
-        help='Configuration file. (This needs its own help section. Note: smrt_bin is deprecated, but if supplied will be appended to PATH.)',
+        help='Configuration file. (TODO: This needs its own help section, or at least a link to a webpage.)',
     )
     args = parser.parse_args(argv[1:])
     return args
