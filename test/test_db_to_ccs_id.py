70e426a3cee65d516e050f14ed50ecbba9c3aa84
Zev Kronenberg <zkronenberg@pacificbiosciences.com>
2019-04-16 20:40:49 -0700

    no more path issues?

--- test/test_db_to_ccs_id.py
+++ test/test_db_to_ccs_id.py
@@ -1,6 +1,9 @@
 import falcon_unzip.mains.db_to_ccs_id as mod
 import pytest
+import helpers
+import os
 import filecmp
+
 
 def test_help():
     try:
@@ -10,17 +13,16 @@ def test_help():
 
 def test_main_1(request):
 
-#    os.path.join(helpers.get_test_data_dir(), 'gfa-1')
-#    print("dir:", request.fspath.dirname)
+    test_data = os.path.join(helpers.get_test_data_dir(), '0-phasing/')
 
     argv = ['prog',
-            '--lookup'      , 'falcon_unzip/test_data/0-phasing/readname_lookup.txt',
-            '--rid-to-phase', 'falcon_unzip/test_data/0-phasing/rid_to_phase.tmp',
-            '--rid-to-ctg'  , 'falcon_unzip/test_data/0-phasing/rid_to_ctg.txt',
+            '--lookup'      , test_data + 'readname_lookup.txt',
+            '--rid-to-phase', test_data + 'rid_to_phase.tmp',
+            '--rid-to-ctg'  , test_data + 'rid_to_ctg.txt',
             '--ctg'         , '000000F',
             '--output'      , 'test_db_to_ccs_id_res.txt'
             ]
 
     mod.main(argv)
 
-    assert(filecmp.cmp('falcon_unzip/test_data/0-phasing/rid_to_phase', 'test_db_to_ccs_id_res.txt'))
+    assert(filecmp.cmp(test_data + 'rid_to_phase', 'test_db_to_ccs_id_res.txt'))
